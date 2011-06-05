254c50e48dfabcd7f04c9f5cdc965c20bcf7d076
Brian Smith <brian@dbsoft.org>
2011-06-05 00:04:51 -0500

    Make the listboxes in the properties inspector expandable.

--- dwib.c
+++ dwib.c
@@ -980,12 +980,12 @@ void DWSIGNAL properties_combobox(xmlNodePtr node)
     dw_window_set_data(vbox, "deftext", item);
     /* List */
     hbox = dw_box_new(DW_HORZ, 0);
-    dw_box_pack_start(scrollbox, hbox, 0, 0, TRUE, FALSE, 0);
+    dw_box_pack_start(scrollbox, hbox, 0, 0, TRUE, TRUE, 0);
     item = dw_text_new("List", 0);
     dw_box_pack_start(hbox, item, PROPERTIES_WIDTH, PROPERTIES_HEIGHT, FALSE, FALSE, 0);
     dw_window_set_style(item, DW_DT_VCENTER, DW_DT_VCENTER);
     item = dw_listbox_new(0, FALSE);
-    dw_box_pack_start(hbox, item, PROPERTIES_WIDTH, 200, TRUE, FALSE, 0);
+    dw_box_pack_start(hbox, item, PROPERTIES_WIDTH, 200, TRUE, TRUE, 0);
     dw_window_set_data(vbox, "list", item);
     if((this = _dwib_find_child(node, "List")))
     {
@@ -1085,12 +1085,12 @@ void DWSIGNAL properties_listbox(xmlNodePtr node)
     
     /* List */
     hbox = dw_box_new(DW_HORZ, 0);
-    dw_box_pack_start(scrollbox, hbox, 0, 0, TRUE, FALSE, 0);
+    dw_box_pack_start(scrollbox, hbox, 0, 0, TRUE, TRUE, 0);
     item = dw_text_new("List", 0);
     dw_box_pack_start(hbox, item, PROPERTIES_WIDTH, PROPERTIES_HEIGHT, FALSE, FALSE, 0);
     dw_window_set_style(item, DW_DT_VCENTER, DW_DT_VCENTER);
     item = dw_listbox_new(0, FALSE);
-    dw_box_pack_start(hbox, item, PROPERTIES_WIDTH, 200, TRUE, FALSE, 0);
+    dw_box_pack_start(hbox, item, PROPERTIES_WIDTH, 200, TRUE, TRUE, 0);
     if((this = _dwib_find_child(node, "List")))
     {
         _dwib_populate_list(item, this, DWDoc);
