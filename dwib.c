6b9b1a9cfdcbf1d131ad0efa0969ab19a31fd6e3
Brian Smith <brian@dbsoft.org>
2011-12-02 00:28:58 -0600

    Enable -1 as options for item size... and add tooltips regarding this.

--- dwib.c
+++ dwib.c
@@ -547,7 +547,8 @@ void properties_item(xmlNodePtr node, HWND scrollbox, int box, int tooltip)
         }
         item = dw_spinbutton_new(val, 0);
         dw_box_pack_start(hbox, item, PROPERTIES_WIDTH/2, PROPERTIES_HEIGHT, TRUE, FALSE, 0);
-        dw_spinbutton_set_limits(item, 2000, 0);
+        dw_spinbutton_set_limits(item, 2000, -1);
+        dw_window_set_tooltip(item, "Set to -1 to calculate the size.");
         dw_window_set_data(vbox, "width", (void *)item);
         val = defvaltrue;
         if((this = _dwib_find_child(node, "height")))
@@ -557,7 +558,8 @@ void properties_item(xmlNodePtr node, HWND scrollbox, int box, int tooltip)
         }
         item = dw_spinbutton_new(val, 0);
         dw_box_pack_start(hbox, item, PROPERTIES_WIDTH/2, PROPERTIES_HEIGHT, TRUE, FALSE, 0);
-        dw_spinbutton_set_limits(item, 2000, 0);
+        dw_spinbutton_set_limits(item, 2000, -1);
+        dw_window_set_tooltip(item, "Set to -1 to calculate the size.");
         dw_window_set_data(vbox, "height", (void *)item);
         /* Expandable */
         hbox = dw_box_new(DW_HORZ, 0);
@@ -3008,6 +3010,7 @@ void DWSIGNAL properties_window(xmlNodePtr node)
     item = dw_spinbutton_new(val, 0);
     dw_box_pack_start(hbox, item, PROPERTIES_WIDTH/2, PROPERTIES_HEIGHT, TRUE, FALSE, 0);
     dw_spinbutton_set_limits(item, 2000, -1);
+    dw_window_set_tooltip(item, "Set to -1 to let the system decide.");
     dw_window_set_data(vbox, "width", (void *)item);
     val = "100";
     if((this = _dwib_find_child(node, "height")))
@@ -3018,6 +3021,7 @@ void DWSIGNAL properties_window(xmlNodePtr node)
     item = dw_spinbutton_new(val, 0);
     dw_box_pack_start(hbox, item, PROPERTIES_WIDTH/2, PROPERTIES_HEIGHT, TRUE, FALSE, 0);
     dw_spinbutton_set_limits(item, 2000, -1);
+    dw_window_set_tooltip(item, "Set to -1 to let the system decide.");
     dw_window_set_data(vbox, "height", (void *)item);
     /* Positon */
     hbox = dw_box_new(DW_HORZ, 0);
@@ -3034,6 +3038,7 @@ void DWSIGNAL properties_window(xmlNodePtr node)
     item = dw_spinbutton_new(val, 0);
     dw_box_pack_start(hbox, item, PROPERTIES_WIDTH/2, PROPERTIES_HEIGHT, TRUE, FALSE, 0);
     dw_spinbutton_set_limits(item, 2000, -1);
+    dw_window_set_tooltip(item, "Set to -1 to let the system decide.");
     dw_window_set_data(vbox, "x", (void *)item);
     val = defvalint;
     if((this = _dwib_find_child(node, "y")))
@@ -3044,6 +3049,7 @@ void DWSIGNAL properties_window(xmlNodePtr node)
     item = dw_spinbutton_new(val, 0);
     dw_box_pack_start(hbox, item, PROPERTIES_WIDTH/2, PROPERTIES_HEIGHT, TRUE, FALSE, 0);
     dw_spinbutton_set_limits(item, 2000, -1);
+    dw_window_set_tooltip(item, "Set to -1 to let the system decide.");
     dw_window_set_data(vbox, "y", (void *)item);
     /* Center */
     hbox = dw_box_new(DW_HORZ, 0);
